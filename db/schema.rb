2000a2d1350197e179ccd8f1c6f648408bd6013f
shraddhu <ssrpatel20@gmail.com>
2024-07-10 17:40:48 -0500

    Add user validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_10_162440) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_10_221550) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_10_162440) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
