c4337840b4418e165ebedf6c5df39206e23df8e4
jaydipPansuriya21 <pansuriyajaydip21@gmail.com>
2024-07-12 21:39:14 +0530

    article created

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,5 @@
 class Article < ApplicationRecord
-    # belongs_to :user
-    # belongs_to :user, optional: true
+    belongs_to :user, optional: true
     validates :title, presence: true, length: {minimum:6, maximum:100}
     validates :description, presence: true, length: {minimum:10, maximum:300}
 end
