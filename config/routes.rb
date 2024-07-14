d4b1ff9459775706d90656975172787f0bf08cfc
shraddhu <ssrpatel20@gmail.com>
2024-07-14 16:55:48 -0500

    User Signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
+  
   resources :articles
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'home', to: 'pages#home'
+  get 'signup', to: 'users#new'
+  resources :users, expect: [:new]    
 
    
 end
